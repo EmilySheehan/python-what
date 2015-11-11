226c7f966f4324a5cd719658cd76d71ddf0cfda4
Emily Sheehan <esheehan@twitter.com>
2015-11-11 16:52:07 -0700

    modify and add

--- learnpy/exflashcards.py
+++ learnpy/exflashcards.py
@@ -1,8 +1,8 @@
 cranberry_song = "Dreaming my Dreams with You"
-lyrics = verse1 + chorusline
-
-verse1 = "All the things you said to me today\nchanged by perspecitve\vin every way"
+verse1 = "All the things you said to me today\nchanged by perspecitve\vin every way\n"
 chorusline = "I\'ll keep dreaming my dreams, with you"
+
+lyrics = verse1 + chorusline
 
 print cranberry_song
 print lyrics
